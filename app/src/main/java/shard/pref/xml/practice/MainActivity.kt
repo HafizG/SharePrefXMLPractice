97356c6c74c98ad14a397c0ebd5570ac1f934f47
hafiz <hafizw008@gmail.com>
2024-09-30 20:24:58 +0500

    Comment added in Main Activity

---
+++ app/src/main/java/shard/pref/xml/practice/MainActivity.kt
@@ -0,0 +1,42 @@
+package shard.pref.xml.practice
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+// Main Activity Code
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fName = findViewById<EditText>(R.id.fNmae)
+        val lName = findViewById<EditText>(R.id.lNmae)
+        val number = findViewById<EditText>(R.id.number)
+        val saveBtn = findViewById<Button>(R.id.saveBtn)
+
+
+
+        saveBtn.setOnClickListener {
+            SharedPrefUtil.setStringValue(NAME, fName.text.toString())
+            SharedPrefUtil.setStringValue(LAST_NAME, lName.text.toString())
+            SharedPrefUtil.setStringValue(PHONE, number.text.toString())
+
+
+            Log.d(
+                "SharedPref", "${SharedPrefUtil.getStringValue(NAME) ?: ""} ${
+                    SharedPrefUtil.getStringValue(
+                        LAST_NAME
+                    ) ?: ""
+                } ${SharedPrefUtil.getStringValue(PHONE) ?: ""} "
+            )
+
+
+            supportFragmentManager.beginTransaction().replace(R.id.container, SetValueFragment())
+                .addToBackStack(null).commit()
+        }
+    }
+}
